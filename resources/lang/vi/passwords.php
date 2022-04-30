548526b8d2813ca348682c04ca4cffee4be2a898
NamBZ <nampt.me@gmail.com>
2022-04-30 20:30:52 +0700

    Make authentication

---
+++ resources/lang/vi/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Mật khẩu mới đã được cập nhật!',
+    'sent' => 'Chúng tôi đã gửi liên kết khôi phục mật khẩu tới Email của bạn!',
+    'throttled' => 'Vui lòng đợi trước khi thử lại.',
+    'token' => 'Mã khôi phục mật khẩu không hợp lệ.',
+    'user' => "Không tìm thấy người dùng với địa chỉ email này.",
+
+];
