e24da3d7f7a2c097770eb55bcdeadb7aba359817
NamBZ <nampt.me@gmail.com>
2022-05-06 09:59:24 +0700

    Search Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,6 +11,13 @@ class Product extends Model
     use HasFactory, SoftDeletes;
 
     /**
+     * The columns of the full text index
+     */
+    protected $searchable = [
+        'title',
+    ];
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
@@ -58,5 +65,50 @@ class Product extends Model
     public function images()
     {
         return $this->morphMany(Image::class, 'imageable');
+    }
+
+    /**
+     * Replaces spaces with full text search wildcards
+     *
+     * @param string $term
+     * @return string
+     */
+    protected function fullTextWildcards($term)
+    {
+        // removing symbols used by MySQL
+        $reservedSymbols = ['-', '+', '<', '>', '@', '(', ')', '~'];
+        $term = str_replace($reservedSymbols, '', $term);
+
+        $words = explode(' ', $term);
+
+        foreach ($words as $key => $word) {
+            /*
+             * applying + operator (required word) only big words
+             * because smaller ones are not indexed by mysql
+             */
+            if (strlen($word) >= 2) {
+                $words[$key] = '+' . $word . '*';
+            }
+        }
+
+        $searchTerm = implode(' ', $words);
+
+        return $searchTerm;
+    }
+
+    /**
+     * Scope a query that matches a full text search of term.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $term
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeFullTextSearch($query, $term)
+    {
+        $columns = implode(',', $this->searchable);
+
+        $query->whereRaw("MATCH ({$columns}) AGAINST (? IN BOOLEAN MODE)", $this->fullTextWildcards($term));
+
+        return $query;
     }
 }
